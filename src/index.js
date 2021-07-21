c5f1cec4a0775fd5c998e291b1309875b7a53be5
Anastasiia kisil <hunterr25n@gmail.com>
2021-07-21 16:37:39 +0400

    scs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import './styles.css';
 import menuContent from './menu.json';
 import menuCardTemplate from './templates/product-card.hbs';
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// Розмітка карток
 const menuRef = document.querySelector('.js-menu');
 const menuMarkup = menuCardTemplate(menuContent);
 menuRef.insertAdjacentHTML('beforeend', menuMarkup);
+
+// Перемикання теми
