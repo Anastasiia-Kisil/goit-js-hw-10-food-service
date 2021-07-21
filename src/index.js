1664d510d4351cf3161b7a22f864e8df9fda362d
Anastasiia kisil <hunterr25n@gmail.com>
2021-07-21 17:31:36 +0400

    11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Всі константи та імпорти
 import './styles.css';
 import menuContent from './menu.json';
 import menuCardTemplate from './templates/product-card.hbs';
@@ -6,10 +7,16 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+const menuRef = document.querySelector('.js-menu');
+const bodyMenu = document.querySelector('body');
+const checkBox = document.querySelector('#theme-switch-toggle');
 
 // Розмітка карток
-const menuRef = document.querySelector('.js-menu');
 const menuMarkup = menuCardTemplate(menuContent);
 menuRef.insertAdjacentHTML('beforeend', menuMarkup);
 
 // Перемикання теми
+checkBox.addEventListener('change', changeTheme);
+function changeTheme(event) {
+  bodyMenu.classList.add('dark-theme');
+}
