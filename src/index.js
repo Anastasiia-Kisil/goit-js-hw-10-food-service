29453d0203dc91bf9c290f8bbf52cc6aa124390a
Anastasiia kisil <hunterr25n@gmail.com>
2021-07-22 12:25:43 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,5 +18,17 @@ menuRef.insertAdjacentHTML('beforeend', menuMarkup);
 // Перемикання теми
 checkBox.addEventListener('change', changeTheme);
 function changeTheme(event) {
-  bodyMenu.classList.add('dark-theme');
+  const checedTheme = checkBox.checked;
+  if (checedTheme) {
+    bodyMenu.classList.add('dark-theme');
+  } else {
+    bodyMenu.classList.add('light-theme');
+    bodyMenu.classList.remove('dark-theme');
+  }
+}
+
+const savedTheme = localStorage.getItem('theme');
+if (['light', 'dark'].includes(savedTheme)) {
+  body.classlist.remove('light-theme', 'dark-theme');
+  body.classList.add(`${savedTheme}-theme`);
 }
